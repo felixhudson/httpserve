f6dac985625b3598c57cad36bb0b34c1d3968a07
Felix Hudson <felix.hudson@adelaide.edu.au>
2018-06-27 17:03:03 +0930

    Before renaming main file

--- httpserve.go
+++ httpserve.go
@@ -5,7 +5,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 )
+
+func myHandler(w http.ResponseWriter, req *http.Request) {
+	log(w, req)
+	fmt.Printf("%v", req.URL)
+	if strings.Contains(req.URL.Path, "jpg") {
+		page := pagestart + showimage(req.URL.Path, "next.jpg") + pageend
+		fmt.Fprint(w, page)
+	} else {
+		fmt.Printf("%v", w)
+		fmt.Printf("%v", req)
+	}
+}
+
+func log(w http.ResponseWriter, req *http.Request) {
+	//fmt.Fprintf(w, "Got request %s %s", req.URL.Path, req.URL.Host)
+	t := string(time.Now().Format("Mon Jan 2 15:04:05 MST 2006"))
+	fmt.Printf("Got request %s %s %s", t, req.URL.Path, req.URL.Host)
+}
 
 func main() {
 	// todo Change to the current directory, or one given on command line
@@ -19,6 +39,7 @@ func main() {
 	mux.HandleFunc("/s2/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "home page 2")
 	})
+	mux.HandleFunc("/test/", myHandler)
 	mux.Handle("/static/", http.StripPrefix("/static", fs))
 	http.ListenAndServe("0.0.0.0:8888", mux)
 }
---
+++ templateconst.go
@@ -0,0 +1,15 @@
+package main
+
+const pagestart = `
+
+<html>
+<body>
+`
+const pageend = `
+</body>
+</html>
+`
+
+func showimage(current string, next string) string {
+	return "<a href='" + next + "<img src='" + current + "'>"
+}
