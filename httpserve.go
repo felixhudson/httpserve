c133bf6e95b8a298a990e6929c165c1c9c558b51
Felix Hudson <felix.hudson@adelaide.edu.au>
2018-06-27 16:13:14 +0930

    Working file server at a static directory

--- httpserve.go
+++ httpserve.go
@@ -3,11 +3,22 @@ package main
 // Inspired by  https://github.com/inconshreveable/srvdir
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+	// todo Change to the current directory, or one given on command line
 	p := http.Dir("/")
 	fs := http.FileServer(p)
-	http.ListenAndServe("0.0.0.0:8888", fs)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "home page")
+	})
+	mux.HandleFunc("/s2/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "home page 2")
+	})
+	mux.Handle("/static/", http.StripPrefix("/static", fs))
+	http.ListenAndServe("0.0.0.0:8888", mux)
 }
